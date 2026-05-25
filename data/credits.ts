3e24fece6ac98b932ac2fdd80d187594ead5c04f
mjulisarchive <mjulis.archive@gmail.com>
2026-05-25 15:55:48 +0900

    Update credits.ts

--- data/credits.ts
+++ data/credits.ts
@@ -15,22 +15,9 @@ export const members: Member[] = [
     email: 'dohyun9041@naver.com'
   },
   {
-    name: '김○○',
-    studentId: '60XXXXXX',
-    role: '콘텐츠 업로드',
-    github: 'GITHUB유저명'
-  },
-  {
-    name: '이○○',
-    studentId: '60XXXXXX',
-    role: '콘텐츠 업로드',
-    email: 'EMAIL'
-  },
-  {
-    name: '최○○',
-    studentId: '60XXXXXX',
-    role: '자료 수집',
-    github: 'GITHUB유저명',
-    email: 'EMAIL'
+    name: '권예슬',
+    studentId: '6023',
+    role: '사이트 개발 및 콘텐츠 관리',
+    email : 'yxsxux@mju.ac.kr'
   }
 ]
