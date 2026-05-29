f8f4b2ab816e03542038bf013ead059ee7058f4c
Domi <dohyun9041@naver.com>
2026-05-29 12:43:18 +0900

    Update credits.ts

--- data/credits.ts
+++ data/credits.ts
@@ -17,25 +17,10 @@ export const members: Member[] = [
     email: 'dohyun9041@naver.com'
   },
   {
-    name: '김○○',
-    studentId: '60XXXXXX',
-    role: '콘텐츠 업로드',
+    name: '권예슬',
+    studentId: '6023',
+    role: '사이트 개발 및 콘텐츠 관리',
     year: '2026 ~ present',
-    github: 'GITHUB유저명'
-  },
-  {
-    name: '이○○',
-    studentId: '60XXXXXX',
-    role: '콘텐츠 업로드',
-    year: '2026 ~ present',
-    email: 'EMAIL'
-  },
-  {
-    name: '최○○',
-    studentId: '60XXXXXX',
-    role: '자료 수집',
-    year: '2026 ~ present',
-    github: 'GITHUB유저명',
-    email: 'EMAIL'
+    email : 'yxsxux@mju.ac.kr'
   }
 ]
